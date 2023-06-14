a4e8e6ae2949a195e736a51d1ca4489f11f7de8a
Nguyen Quy Hoang Ky <kynguyen5698@gmail.com>
2023-06-14 17:33:36 +0700

    Create YoutubeData.js

---
+++ src/YoutubeData.js
@@ -0,0 +1,30 @@
+export const YoutubeData = [
+    {
+        id: 1,
+        image: "https://images.unsplash.com/photo-1682687220866-c856f566f1bd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
+        avatar: "https://images.unsplash.com/photo-1686676844352-bd0f1fdf3465?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
+        title: "Title 1",
+        author: "Author 1",
+    },
+    {
+        id: 2,
+        image: "https://images.unsplash.com/photo-1686690279310-1d24e5be10f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        avatar: "https://images.unsplash.com/photo-1686563873984-9ee2048d1c52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        title: "Title 2",
+        author: "Author 2",
+    },
+    {
+        id: 3,
+        image: "https://images.unsplash.com/photo-1685703206522-9c9c0d03bce4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        avatar: "https://plus.unsplash.com/premium_photo-1686653830755-c455c730ad92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        title: "Title 3",
+        author: "Author 3",
+    },
+    {
+        id: 4,
+        image: "https://images.unsplash.com/photo-1686630079100-fbbe083d2bd4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        avatar: "https://images.unsplash.com/photo-1686659569920-28159b5fe4f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        title: "Title 4",
+        author: "Author 4",
+    },
+]
