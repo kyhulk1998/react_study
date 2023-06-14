99cade0d4879a2cc49e76a2c0843085ff1055980
nqhky <nguyen149@avaya.com>
2023-06-14 16:16:00 +0700

    Rendering List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { YoutubeData } from './YoutubeData';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="youtube-list">
+        {YoutubeData.map((item, index) => (
+            <YoutubeItem key={item.id} image={item.image} avatar={item.avatar} title={item.title} author={item.author}></YoutubeItem>    
+        ))}
+        </div>
+    );
+}
+
+function YoutubeItem(props) {
+    return ( 
+        <div className="youtube-item">
+            <div className="youtube-image">
+                <img src={props.image} alt="" />
+            </div>
+            <div className="youtube-footer">
+                <img src={props.avatar} alt="" className="youtube-avatar" />
+                <div className="youtube-info">
+                    <h2 className="youtube-title">{props.title || "This is title"}</h2>
+                    <p className="youtube-author">{props.author || "This is author"}</p>
+                </div>
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
